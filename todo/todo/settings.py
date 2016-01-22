5a6764d0551ffae76723a39a85ab7200d26d9d31
Zach Goldberg <zach@zachgoldberg.com>
2016-01-21 17:28:40 -0800

    Documentation URLs

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework_swagger',
     'rest_framework',
     'rest_framework.authtoken'
 ]
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
